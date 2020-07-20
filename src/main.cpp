1a9508a5f041f32e7dbab76f86f77478c021e723
ksato-dev <k-sato@smartrobotics.jp>
2020-07-20 12:02:35 +0900

    test using my structure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-int hoge = 1;
-int piyo = 10;
+struct TaskParams
+{
+  int16_t hoge;
+  int16_t piyo;
+};
 
 TaskHandle_t th[2];
+TaskParams tParams;
+
+void initialize_params(TaskParams &params)
+{
+  params.hoge = 0;
+  params.piyo = 0;
+}
 
 void test(void *param)
 {
@@ -17,17 +27,35 @@ void test(void *param)
   }
 }
 
+void test_for_structure(void *param)
+{
+  while(1)
+  {
+    TaskParams *temp_param = (TaskParams *)param;
+
+    (*temp_param).hoge += 1;
+    (*temp_param).piyo -= 1;
+    Serial.print("hoge in test():" + String((*temp_param).hoge));
+    Serial.println(", piyo in test():" + String((*temp_param).piyo));
+    delay(100);
+  }
+}
+
 void setup()
 {
+  initialize_params(tParams);
   // put your setup code here, to run once:
   Serial.begin(115200);
   // xTaskCreatePinnedToCore(test, "test", 4096, NULL, 10, &th[0], 0);
-  xTaskCreatePinnedToCore(test, "test", 4096, &hoge, 10, &th[0], 0);
+  // xTaskCreatePinnedToCore(test, "test", 4096, &hoge, 10, &th[0], 0);
+  xTaskCreatePinnedToCore(test_for_structure, "test_for_structure", 4096, &tParams, 10, &th[0], 0);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println("hoge in loop():" + String(hoge));
+  // Serial.println("hoge in loop():" + String(hoge));
+  Serial.print("hoge in loop():" + String(tParams.hoge));
+  Serial.println(", piyo in loop():" + String(tParams.piyo));
   delay(1000);
 }
